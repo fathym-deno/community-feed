95efa695808ef69b723d86ce6c8d4fe5dd625924
michael.gearhardt@fathym.com <michael.gearhardt@fathym.com>
2023-08-22 05:36:49 +0000

    Update imports, add Atomic Icons plugin, and update Button component for Fathym integration.

--- www/components/Button.tsx
+++ www/components/Button.tsx
@@ -1,12 +1,11 @@
-import { JSX } from "preact";
-import { IS_BROWSER } from "$fresh/runtime.ts";
+import { CheckCircleIcon, ExclaimIcon, Icon, XCircleIcon } from "@atomic/design";
 
-export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
+export default function Button() {
   return (
-    <button
-      {...props}
-      disabled={!IS_BROWSER || props.disabled}
-      class="px-2 py-1 border-gray-500 border-2 rounded bg-white hover:bg-gray-200 transition-colors"
-    />
+    <>
+      <CheckCircleIcon />
+      <ExclaimIcon />
+      <XCircleIcon />
+    </>
   );
 }
