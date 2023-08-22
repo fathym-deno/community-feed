eb7fdc30fe79fa1ad30154d38e576a70f87dbc31
mcgear <michael.gearhardt@gmail.com>
2023-08-22 00:06:49 -0600

    Working with Design kit

--- www/islands/Counter.tsx
+++ www/islands/Counter.tsx
@@ -1,6 +1,6 @@
 import type { Signal } from "@preact/signals";
-import { Button } from "../components/Button.tsx";
 import { AddIcon, SubtractIcon } from "$fathym/atomic-icons";
+import { Action } from "@atomic/design";
 
 interface CounterProps {
   count: Signal<number>;
@@ -9,19 +9,19 @@ interface CounterProps {
 export default function Counter(props: CounterProps) {
   return (
     <div class="flex gap-8 py-6">
-      <Button onClick={() => props.count.value -= 1}>
+      <Action onClick={() => props.count.value -= 1}>
         <>
           <SubtractIcon class="w-[24px] h-[24px] inline" />
           1
         </>
-      </Button>
+      </Action>
       <p class="text-3xl">{props.count}</p>
-      <Button onClick={() => props.count.value += 1}>
+      <Action onClick={() => props.count.value += 1}>
         <>
           {/* <AddIcon class="w-[24px] h-[24px] inline" /> */}
           1
         </>
-      </Button>
+      </Action>
     </div>
   );
 }
