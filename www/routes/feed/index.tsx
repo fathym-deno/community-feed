eb19531f4049b44a541a2ca9ab7c29c415427078
mcgear <michael.gearhardt@gmail.com>
2023-08-24 01:04:50 -0600

    Cleanup

--- www/routes/feed/index.tsx
+++ www/routes/feed/index.tsx
@@ -17,20 +17,20 @@ import { Icon } from "$fathym/atomic-icons";
 import { Handlers, PageProps } from "$fresh/server.ts";
 import { handler as feedSvc } from "../api/feed.ts";
 
+const actionStyles = {
+  actionStyle: ActionStyleTypes.None,
+  class: "flex-grow flex",
+};
+
+const iconStyles = {
+  class: "w-[24px] h-[24px] mr-2",
+};
+
 function loadFeedItems(feedData: any[]): BuildFeedCardProps[] {
   return feedData.map((item: any, index: number) => {
     const avatar = item.Contributors
       ? item.Contributors[0].UserImage
       : item.Avatar;
-
-    const actionStyles = {
-      actionStyle: ActionStyleTypes.None,
-      class: "flex-grow flex",
-    };
-
-    const iconStyles = {
-      class: "w-[24px] h-[24px] ml-2",
-    };
 
     const actions = item.Actions.map((action: any) => {
       if (action.Action.startsWith("./")) {
@@ -61,13 +61,11 @@ function loadFeedItems(feedData: any[]): BuildFeedCardProps[] {
 
     const tabs = item.Tabs
       ? item.Tabs.map((tab: any) => {
-          return {
-            label: tab.Title,
-            content: (
-              <div dangerouslySetInnerHTML={{ __html: tab.Content }} />
-            ),
-          } as Tab;
-        })
+        return {
+          label: tab.Title,
+          content: <div dangerouslySetInnerHTML={{ __html: tab.Content }} />,
+        } as Tab;
+      })
       : undefined;
 
     return {
@@ -89,8 +87,10 @@ function loadAdvertisements(feedData: any[]): BuildFeedCardProps[] {
   return feedData.map((item: any) => {
     const actions = item.Actions.map((action: any) => {
       return {
-        actionStyle: ActionStyleTypes.Solid | ActionStyleTypes.Rounded | ActionStyleTypes.Link,
+        actionStyle: ActionStyleTypes.Solid | ActionStyleTypes.Rounded |
+          ActionStyleTypes.Link,
         href: action.Action,
+        class: "flex",
         children: (
           <>
             <Icon {...iconStyles} icon={action.Icon} src="./iconset/icons" />
@@ -124,7 +124,9 @@ export const handler: Handlers = {
 };
 
 export default function Feed(props: PageProps): JSX.Element {
-  const [data, setData] = useState<BuildFeedCardProps[]>([]);
+  const [data, setData] = useState<
+    { items: BuildFeedCardProps[]; advertisements: BuildFeedCardProps[] }
+  >({ items: [], advertisements: [] });
 
   setData(props.data);
 
@@ -147,53 +149,9 @@ export default function Feed(props: PageProps): JSX.Element {
   };
 
   const postFormProps: PostFormProps = {
-    class: "mt-4 md:mt-8",
+    class: "m-4 md:m-8",
     avatar: "https://github.com/mcgear.png",
   };
-
-  const secondColumnData = [
-    {
-      Actions: [
-        {
-          Action: "/dashboard/create-project",
-          ActionType: "Link",
-          Color: "",
-          Icon: "add",
-          Text: "Create Project",
-        },
-      ],
-      Description:
-        "Build trust with your audience and drive traffic to your site.",
-      Image: "https://www.fathym.com/img/questions/dotcomcircle.png",
-      Lead: "Strengthen your brand with a custom domain",
-      Position: "End",
-    },
-  ];
-
-  const secondColumnCards = secondColumnData.map((item) => {
-    const actions = item.Actions.map((action: any) => {
-      return {
-        actionStyle: ActionStyleTypes.Solid | ActionStyleTypes.Rounded |
-          ActionStyleTypes.Link,
-        href: action.Action,
-        children: (
-          <>
-            <Icon {...iconStyles} icon={action.Icon} src="./iconset/icons" />
-            {action.Text}
-          </>
-        ),
-      };
-    });
-
-    return (
-      <FeedCard
-        title={item.Description}
-        subtitle={item.Lead}
-        avatar={item.Image}
-        actions={actions}
-      />
-    );
-  });
 
   const postWithFeedProps: PostWithFeedProps = {
     feedCardListProps: feedCardListProps,
@@ -201,11 +159,12 @@ export default function Feed(props: PageProps): JSX.Element {
   };
 
   return (
-    <div class="max-w-screen-md mx-auto flex">
-      <PostWithFeed {...postWithFeedProps} />
-      <div class="ml-4 md:ml-8">
-        <FeedCardList cards={secondColumnCards}></FeedCardList>
-      </div>
+    <div class="mx-auto flex justify-center align-middle">
+      <PostWithFeed {...postWithFeedProps} class="max-w-screen-md" />
+      <FeedCardList
+        cards={data.advertisements}
+        class="m-4 md:m-8 w-[350px]"
+      />
     </div>
   );
 }
