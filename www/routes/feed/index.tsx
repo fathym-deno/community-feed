52a917be793f87c80057bbaac9ab9d2817b3f15f
michael.gearhardt@fathym.com <michael.gearhardt@fathym.com>
2023-08-24 06:21:29 +0000

    "Add second column with FeedCards to feed page in www\routes\feed\index.tsx"

--- www/routes/feed/index.tsx
+++ www/routes/feed/index.tsx
@@ -4,6 +4,8 @@ import {
   ActionStyleTypes,
   BuildFeedCard,
   BuildFeedCardProps,
+  FeedCard,
+  FeedCardList,
   FeedCardListProps,
   PostFormProps,
   PostWithFeed,
@@ -121,6 +123,48 @@ export default function Feed(props: PageProps): JSX.Element {
     avatar: "https://github.com/mcgear.png",
   };
 
+  const secondColumnData = [
+    {
+      Actions: [
+        {
+          Action: "/dashboard/create-project",
+          ActionType: "Link",
+          Color: "",
+          Icon: "add",
+          Text: "Create Project",
+        },
+      ],
+      Description: "Build trust with your audience and drive traffic to your site.",
+      Image: "https://www.fathym.com/img/questions/dotcomcircle.png",
+      Lead: "Strengthen your brand with a custom domain",
+      Position: "End",
+    },
+  ];
+
+  const secondColumnCards = secondColumnData.map((item) => {
+    const actions = item.Actions.map((action: any) => {
+      return {
+        actionStyle: ActionStyleTypes.Solid | ActionStyleTypes.Rounded | ActionStyleTypes.Link,
+        href: action.Action,
+        children: (
+          <>
+            <Icon {...iconStyles} icon={action.Icon} src="./iconset/icons" />
+            {action.Text}
+          </>
+        ),
+      };
+    });
+
+    return (
+      <FeedCard
+        title={item.Description}
+        subtitle={item.Lead}
+        avatar={item.Image}
+        actions={actions}
+      />
+    );
+  });
+
   const postWithFeedProps: PostWithFeedProps = {
     feedCardListProps: feedCardListProps,
     postFormProps: postFormProps,
@@ -129,6 +173,9 @@ export default function Feed(props: PageProps): JSX.Element {
   return (
     <div class="max-w-screen-md mx-auto flex">
       <PostWithFeed {...postWithFeedProps} />
+      <div class="ml-4 md:ml-8">
+        <FeedCardList>{secondColumnCards}</FeedCardList>
+      </div>
     </div>
   );
 }
